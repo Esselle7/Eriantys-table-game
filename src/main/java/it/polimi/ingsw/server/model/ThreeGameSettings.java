86ee49c2cefc17aaa701f26bf16c5e55437791ab
Simone <simoone1.leone@mail.polimi.it>
2022-04-02 13:45:24 +0200

    Create two and three players game settings

---
+++ src/main/java/it/polimi/ingsw/server/model/ThreeGameSettings.java
@@ -0,0 +1,21 @@
+package it.polimi.ingsw.server.model;
+
+/**
+ * This class store the settings
+ * for three players
+ */
+
+public class ThreeGameSettings extends GameSettings{
+
+    /**
+     * This method allow to set
+     * the settings for two players
+     */
+    @Override
+    public void manageSettings() {
+        setStudentsCloudTile(4);
+        setTowerYard(6);
+        setStudentsEntranceRoom(9);
+        setDiningRoomLenght(10);
+    }
+}
---
+++ src/main/java/it/polimi/ingsw/server/model/TwoGameSettings.java
@@ -0,0 +1,21 @@
+package it.polimi.ingsw.server.model;
+
+/**
+ * This class store the settings
+ * for three players
+ */
+
+public class TwoGameSettings extends GameSettings{
+
+    /**
+     * This method allow to set
+     * the settings for two players
+     */
+    @Override
+    public void manageSettings() {
+        setStudentsCloudTile(3);
+        setTowerYard(8);
+        setStudentsEntranceRoom(7);
+        setDiningRoomLenght(10);
+    }
+}
