a9a9da772d5dcd93ae158c8d88ce2cdd344e79ef
Simone <simoone1.leone@mail.polimi.it>
2022-04-02 13:45:00 +0200

    Create basic abstract class for settings

---
+++ src/main/java/it/polimi/ingsw/server/model/GameSettings.java
@@ -0,0 +1,55 @@
+package it.polimi.ingsw.server.model;
+
+/**
+ * This abstract class will contain
+ * the main information for two players
+ * mode and three players mode
+ */
+
+public abstract class GameSettings {
+    private int studentsEntranceRoom;
+    private int towerYard;
+    private int studentsCloudTile;
+    private int diningRoomLenght;
+
+    /**
+     * This method will be implemented
+     * in the two classes that extends
+     * this abstract class. It allows
+     * to modify the game settings
+     * (the attributes)
+     */
+    public abstract void manageSettings();
+
+    public int getDiningRoomLenght() {
+        return diningRoomLenght;
+    }
+
+    public void setDiningRoomLenght(int diningRoomLenght) {
+        this.diningRoomLenght = diningRoomLenght;
+    }
+
+    public int getStudentsEntranceRoom() {
+        return studentsEntranceRoom;
+    }
+
+    public void setStudentsEntranceRoom(int studentsEntranceRoom) {
+        this.studentsEntranceRoom = studentsEntranceRoom;
+    }
+
+    public int getTowerYard() {
+        return towerYard;
+    }
+
+    public void setTowerYard(int towerYard) {
+        this.towerYard = towerYard;
+    }
+
+    public int getStudentsCloudTile() {
+        return studentsCloudTile;
+    }
+
+    public void setStudentsCloudTile(int studentsCloudTile) {
+        this.studentsCloudTile = studentsCloudTile;
+    }
+}
