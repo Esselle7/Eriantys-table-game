f022cd14d969b3720524830c2b1231c8acd0d4cb
Simone <simoone1.leone@mail.polimi.it>
2022-04-02 15:06:07 +0200

    Update the JavaDoc

--- src/main/java/it/polimi/ingsw/server/model/CloudTile.java
+++ src/main/java/it/polimi/ingsw/server/model/CloudTile.java
@@ -1,5 +1,13 @@
 package it.polimi.ingsw.server.model;
 
+/**
+ * Class that represent
+ * a cloud tile of the game.
+ * It is composed by the students
+ * that a player at each turn can
+ * take
+ */
+
 public class CloudTile {
     private int[] students;
 
--- src/main/java/it/polimi/ingsw/server/model/PlayGround.java
+++ src/main/java/it/polimi/ingsw/server/model/PlayGround.java
@@ -3,6 +3,12 @@ package it.polimi.ingsw.server.model;
 import java.util.List;
 import java.util.Objects;
 
+/**
+ * Class that store every
+ * instance of the class in order to
+ * play a game.
+ */
+
 public class PlayGround {
     private List<Player> playersList;
     private List<Island> islands;
@@ -10,6 +16,14 @@ public class PlayGround {
     private CloudTile[] cloudTiles;
     private static final PlayGround instance = null;
 
+    /**
+     * Private constructor, applied
+     * singleton pattern
+     * @param playersList list of the current players
+     * @param islands list of the islands instances
+     * @param cloudTiles list of cloud tile with students
+     *                   already on it
+     */
     private PlayGround(List<Player> playersList, List<Island> islands, CloudTile[] cloudTiles) {
         this.playersList = playersList;
         this.islands = islands;
@@ -17,6 +31,17 @@ public class PlayGround {
         professorsControl = new Player[Colour.colourCount];
     }
 
+    /**
+     * Public static class that create
+     * an instance of playground only
+     * if there are no others object that
+     * has been instanced before
+     * @param playersList list of the current players
+     * @param islands list of the islands instances
+     * @param cloudTiles list of cloud tile with students
+     *      *            already on it
+     * @return the playGround instance
+     */
     public static PlayGround createPlayground(List<Player> playersList, List<Island> islands, CloudTile[] cloudTiles)
     {
         return Objects.requireNonNullElseGet(instance, () -> new PlayGround(playersList, islands, cloudTiles));
