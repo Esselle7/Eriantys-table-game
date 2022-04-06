85464c6f07999507db2c2759c0708284b5ccb91f
Simone <simoone1.leone@mail.polimi.it>
2022-04-06 17:14:35 +0200

    Create main classes for controller to implement

---
+++ src/main/java/it/polimi/ingsw/server/controller/GameController.java
@@ -0,0 +1,38 @@
+package it.polimi.ingsw.server.controller;
+import it.polimi.ingsw.server.model.GameSettings;
+import it.polimi.ingsw.server.model.PlayGround;
+
+import java.util.List;
+
+
+public class GameController {
+    private PlayGround currentGame;
+    private GameSettings currentSettings;
+    private IslandController islandController;
+    private BoardController boardController;
+
+    public GameController(GameSettings settings)
+    {
+        currentSettings = settings;
+        islandController = new IslandController();
+        boardController = new BoardController();
+    }
+
+    public void setUpGame()
+    {
+        // qui richiamiamo tutti i controller per fare i set up
+
+    }
+
+    private void setUpPlayers(int numberOfPlayers)
+    {
+
+    }
+
+    private List setUpIslands()
+    {
+        return null; // da modificare, l'ho messo solo per poter committare
+    }
+
+
+}
---
+++ src/main/java/it/polimi/ingsw/server/controller/IslandController.java
@@ -0,0 +1,14 @@
+package it.polimi.ingsw.server.controller;
+
+import it.polimi.ingsw.server.model.Island;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class IslandController {
+    public IslandController()
+    {
+    }
+
+
+}
