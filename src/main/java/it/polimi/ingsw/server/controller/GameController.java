10e7e48270f0f292e317829fe4676c913d213126
Simone <simoone1.leone@mail.polimi.it>
2022-04-08 10:21:45 +0200

    Add TurnHandler to manage players

--- src/main/java/it/polimi/ingsw/server/controller/GameController.java
+++ src/main/java/it/polimi/ingsw/server/controller/GameController.java
@@ -1,5 +1,7 @@
 package it.polimi.ingsw.server.controller;
+import it.polimi.ingsw.server.model.Colour;
 import it.polimi.ingsw.server.model.GameSettings;
+import it.polimi.ingsw.server.model.Island;
 import it.polimi.ingsw.server.model.PlayGround;
 
 import java.util.List;
@@ -7,30 +9,27 @@ import java.util.List;
 
 public class GameController {
     private PlayGround currentGame;
+    private TurnHandler turnHandler;
     private GameSettings currentSettings;
     private IslandController islandController;
     private BoardController boardController;
 
-    public GameController(GameSettings settings)
-    {
+    public GameController(GameSettings settings) {
         currentSettings = settings;
         islandController = new IslandController();
         boardController = new BoardController();
     }
 
-    public void setUpGame()
-    {
+    public void setUpGame() {
         // qui richiamiamo tutti i controller per fare i set up
 
     }
 
-    private void setUpPlayers(int numberOfPlayers)
-    {
+    private void setUpPlayers(int numberOfPlayers) {
 
     }
 
-    private List setUpIslands()
-    {
+    private List setUpIslands() {
         return null; // da modificare, l'ho messo solo per poter committare
     }
 
