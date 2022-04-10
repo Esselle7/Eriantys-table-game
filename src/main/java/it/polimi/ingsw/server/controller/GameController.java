e78fa978946240fea6871787eec070c884c1b9aa
Simone <simoone1.leone@mail.polimi.it>
2022-04-10 09:21:45 +0200

    Implement "move student to island" move

--- src/main/java/it/polimi/ingsw/server/controller/GameController.java
+++ src/main/java/it/polimi/ingsw/server/controller/GameController.java
@@ -1,6 +1,5 @@
 package it.polimi.ingsw.server.controller;
-import it.polimi.ingsw.server.model.GameSettings;
-import it.polimi.ingsw.server.model.PlayGround;
+import it.polimi.ingsw.server.model.*;
 
 import java.util.List;
 
@@ -10,12 +9,14 @@ public class GameController {
     private GameSettings currentSettings;
     private IslandController islandController;
     private BoardController boardController;
+    private TurnHandler turnHandler;
 
     public GameController(GameSettings settings)
     {
         currentSettings = settings;
         islandController = new IslandController();
         boardController = new BoardController();
+
     }
 
     public void setUpGame()
@@ -29,10 +30,51 @@ public class GameController {
 
     }
 
-    private List setUpIslands()
-    {
-        return null; // da modificare, l'ho messo solo per poter committare
+    public PlayGround getCurrentGame() {
+        return currentGame;
     }
 
+    public void setCurrentGame(PlayGround currentGame) {
+        this.currentGame = currentGame;
+    }
+
+    public GameSettings getCurrentSettings() {
+        return currentSettings;
+    }
+
+    public void setCurrentSettings(GameSettings currentSettings) {
+        this.currentSettings = currentSettings;
+    }
+
+    public IslandController getIslandController() {
+        return islandController;
+    }
+
+    public void setIslandController(IslandController islandController) {
+        this.islandController = islandController;
+    }
+
+    public BoardController getBoardController() {
+        return boardController;
+    }
+
+    public void setBoardController(BoardController boardController) {
+        this.boardController = boardController;
+    }
+
+    public TurnHandler getTurnHandler() {
+        return turnHandler;
+    }
+
+    public void setTurnHandler(TurnHandler turnHandler) {
+        this.turnHandler = turnHandler;
+    }
+    
+    public void moveStudentsEntranceToIsland(int studentColour, int selectedIsland)
+    {
+       turnHandler.getCurrentPlayer().getPlayerBoard().removeStudentEntrance(studentColour);
+       currentGame.getIslandByIndex(selectedIsland).setPlacedStudent(studentColour);
+
+    }
 
 }
