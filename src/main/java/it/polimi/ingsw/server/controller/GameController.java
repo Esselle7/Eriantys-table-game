00f2f822283c7ddaab8bd49373c494ab7ae07c1b
Simone <simoone1.leone@mail.polimi.it>
2022-04-10 18:20:07 +0200

    Implement moves in board and from board to island

--- src/main/java/it/polimi/ingsw/server/controller/GameController.java
+++ src/main/java/it/polimi/ingsw/server/controller/GameController.java
@@ -1,10 +1,8 @@
 package it.polimi.ingsw.server.controller;
 import it.polimi.ingsw.server.model.*;
 
-import java.util.List;
 
-
-public class GameController {
+public class GameController extends ManagerStudent{
     private PlayGround currentGame;
     private GameSettings currentSettings;
     private IslandController islandController;
@@ -69,12 +67,65 @@ public class GameController {
     public void setTurnHandler(TurnHandler turnHandler) {
         this.turnHandler = turnHandler;
     }
-    
+
+    private Board getCurrentPlayerBoard() //shortcut
+    {
+        return getTurnHandler().getCurrentPlayerBoard();
+    }
+
     public void moveStudentsEntranceToIsland(int studentColour, int selectedIsland)
     {
-       turnHandler.getCurrentPlayer().getPlayerBoard().removeStudentEntrance(studentColour);
-       currentGame.getIslandByIndex(selectedIsland).setPlacedStudent(studentColour);
+        getCurrentPlayerBoard().removeStudentEntrance(studentColour);
+        getCurrentGame().getIslandByIndex(selectedIsland).setPlacedStudent(studentColour);
 
     }
 
-}
+    public void moveStudentEntranceToDining(int studentColour)
+    {
+        getCurrentPlayerBoard().removeStudentEntrance(studentColour);
+        getCurrentPlayerBoard().increaseNumberOfStudent(studentColour);
+    }
+
+    public void setInfluenceToIsland()
+    {
+        getCurrentPlayerBoard().decreaseTowerYard();
+        getCurrentGame().getIslandWithMotherNature().setInfluence();
+        getCurrentGame().getIslandWithMotherNature().setTowerColour(getCurrentPlayerBoard().getTowerColour());
+    }
+
+    public void changeInfluenceToIsland()
+    {
+        getCurrentPlayerBoard().decreaseTowerYard();
+        getCurrentGame().getPlayersList().stream().filter(p -> p.getPlayerBoard().getTowerColour().equals(getCurrentGame().getIslandWithMotherNature().getTowerColour())).forEachOrdered(p -> p.getPlayerBoard().increaseTowerYard()); //add tower to previous island owner
+        getCurrentGame().getIslandWithMotherNature().setTowerColour(getCurrentPlayerBoard().getTowerColour());
+
+    }
+
+    public void takeStudentsFromCloudTile(int chosenCloudTile) // you have to control isUsed attribute in CloudTile before calling this method
+    {
+        getCurrentPlayerBoard().setEntranceRoom(addStudentsToTarget(getCurrentPlayerBoard().getEntranceRoom(),getCurrentGame().getCloudTiles()[chosenCloudTile].getStudents()));
+    }
+
+    public void useAssistantCard(int cardNumber)
+    {
+        if(checkCardValidity(cardNumber))
+            getTurnHandler().getCurrentPlayer().useCard(cardNumber);
+        // else throw an exception
+
+    }
+
+    private boolean checkCardValidity(int cardNumber)
+    {
+        for (Player player: getCurrentGame().getPlayersList()) {
+            if(player.getNickname().equals(getTurnHandler().getCurrentPlayer().getNickname()))
+                return true;
+            if(player.getCurrentCard().getValue() == cardNumber)
+                return false;
+        }
+        return false;
+    }
+
+
+
+
+} // ottimizza codice con scrittura di metodi in turn handler
