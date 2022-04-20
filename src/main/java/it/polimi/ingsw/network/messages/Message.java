b5ce91204a8156d53e39e456a409773d00b0c9a9
Simone <simoone1.leone@mail.polimi.it>
2022-04-20 09:58:46 +0200

    Create the message interface and ping class

---
+++ src/main/java/it/polimi/ingsw/network/messages/Message.java
@@ -0,0 +1,6 @@
+package it.polimi.ingsw.network.messages;
+
+import java.io.Serializable;
+
+public interface Message extends Serializable {
+}
---
+++ src/main/java/it/polimi/ingsw/network/messages/Ping.java
@@ -0,0 +1,4 @@
+package it.polimi.ingsw.network.messages;
+
+public class Ping implements Message {
+}
