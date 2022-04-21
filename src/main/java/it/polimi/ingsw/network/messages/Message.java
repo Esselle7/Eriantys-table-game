64c7b871082cd2748fdf308bf8a6ea74d128aa96
Simone <simoone1.leone@mail.polimi.it>
2022-04-21 15:55:56 +0200

    Add java doc to class Message

--- src/main/java/it/polimi/ingsw/network/messages/Message.java
+++ src/main/java/it/polimi/ingsw/network/messages/Message.java
@@ -2,5 +2,10 @@ package it.polimi.ingsw.network.messages;
 
 import java.io.Serializable;
 
+/**
+ * This is a generic interface for messages
+ * that allows to create multiple static serializable
+ * object
+ */
 public interface Message extends Serializable {
 }
