ba9592a86b3d628503d056f407e4fdad6a01e0e9
Simone <simoone1.leone@mail.polimi.it>
2022-04-23 09:11:31 +0200

    create method to set up a message listener

--- src/main/java/it/polimi/ingsw/client/Client.java
+++ src/main/java/it/polimi/ingsw/client/Client.java
@@ -1,6 +1,9 @@
 package it.polimi.ingsw.client;
 
 import it.polimi.ingsw.client.cli.Cli;
+import it.polimi.ingsw.client.connection.ConnectionClientSide;
+import it.polimi.ingsw.client.connection.TCPClientSideConnection;
+
 import java.io.IOException;
 import java.net.Socket;
 import java.util.Scanner;
@@ -14,18 +17,32 @@ public class Client {
     public static void main(String[] args) {
         Socket socket = null;
         boolean connected = false;
+
         chooseView();
         ui.loadView();
-
         while (!connected) {
             try {
                 socket = new Socket(ui.getServerAddress(),ui.getServerPort());
                 connected = true;
                 ui.printText("Connected! Waiting for a game...");
             } catch (IOException e) {
-                ui.printText("Could not connect to the server. Retrying...");
+                ui.printText("Could not connect to the server. Please try again later.");
+                System.exit(-1);
             }
         }
+
+        ConnectionClientSide currentConnection;
+        ClientProcessingCommands clientProcessingCommands = null;
+        try {
+            currentConnection = new TCPClientSideConnection(socket);
+            clientProcessingCommands = new ClientProcessingCommands(currentConnection, ui);
+        } catch (IOException e) {
+            ui.printText("Error connecting to the server. Please try again later.");
+            System.exit(-1);
+        }
+        Thread messageListener = new Thread(clientProcessingCommands);
+        messageListener.start();
+
 
 
     }
