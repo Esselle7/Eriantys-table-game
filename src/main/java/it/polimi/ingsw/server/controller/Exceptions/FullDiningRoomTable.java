9fc0ced8f9aa7bdbed6a45a0cd6bb8e0d0631f3a
Simone <simoone1.leone@mail.polimi.it>
2022-04-25 09:57:13 +0200

    Add javaDoc to exceptions

--- src/main/java/it/polimi/ingsw/server/controller/Exceptions/FullDiningRoomTable.java
+++ src/main/java/it/polimi/ingsw/server/controller/Exceptions/FullDiningRoomTable.java
@@ -1,5 +1,9 @@
 package it.polimi.ingsw.server.controller.Exceptions;
 
+/**
+ * This exception represent that a player is trying to add a
+ * student to a full dining room table
+ */
 public class FullDiningRoomTable extends Exception{
     public FullDiningRoomTable(){}
 }
